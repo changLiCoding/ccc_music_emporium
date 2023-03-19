847046b4b358d30d3e38134a087124b0cbb857ee
Cody Hilborn <codyhilborn11@gmail.com>
2023-03-19 02:47:03 -0400

    Add detailed products to seed file, change subcategories

--- server/db/seeds/04_sub_categories.sql
+++ server/db/seeds/04_sub_categories.sql
@@ -1,9 +1,12 @@
 INSERT INTO sub_categories (category_id, name) VALUES
 (1,'acoustic guitars'),
 (1, 'electric guitars'),
+(1, 'bass guitars'),
 (2, 'digital pianos'),
-(2, 'keyboard amplifications'),
-(3, 'acoustic drums & sets'),
-(3, 'electronic drums & sets'),
-(4, 'acoustic treatment'),
-(4, 'dj');
+(2, 'MIDI keyboards'),
+(3, 'acoustic drum kits'),
+(3, 'electronic drum kits'),
+(4, 'guitar amps'),
+(4, 'bass amps'),
+(4, 'effects pedals');
+
