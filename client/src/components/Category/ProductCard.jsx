7784b90413a6fb90ab0da66e53a437da0f650b5f
Can Kologlu <mckologlu@gmail.com>
2023-03-20 19:50:49 -0400

    fixed css on products page and add conditional rendering

--- client/src/components/Category/ProductCard.jsx
+++ client/src/components/Category/ProductCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Card(props) {
 	const { product, category } = props;
-	return (	
-
-		category === 'guitars' && <div className='card card-side w-96 bg-white shadow hover:shadow-3xl transition-all justify-self-center border 1px flex items-stretch'>
+	console.log("category is:",category)
+	return category === "guitars"?(
+	<div className='card card-side w-96 bg-white shadow hover:shadow-3xl 	transition-all'>
 			<figure>
 				<img
 					className='-rotate-90'
@@ -12,7 +12,6 @@ export default function Card(props) {
 					alt={product.model}
 				/>
 			</figure>
-			
 			<div className='card-body'>
 				<h2 className='card-title'>
 					{product.model}
@@ -30,6 +29,21 @@ export default function Card(props) {
 				</div>
 			</div>
 		</div>
-
+		)
+		:(<div className="card w-96 w-96 bg-white shadow hover:shadow-3xl 	transition-all">
+		<figure className = "max-w-52 max-h-52"><img className="object-contain h-48" src={product.image_url} alt={product.model} /></figure>
+		<div className="card-body">
+			<h2 className="card-title">
+			{product.model}
+				<div className="badge badge-secondary">NEW</div>
+			</h2>
+			<p>{product.make}</p>
+			<div className="card-actions justify-end">
+				<div className="badge badge-outline">{category.charAt(0).toUpperCase() + category.slice(1)}</div> 
+				<div className="badge badge-outline">{product.sub_category_name.charAt(0).toUpperCase() +
+							product.sub_category_name.slice(1)}</div>
+			</div>
+		</div>
+	</div>
 	);
 }
