a832364ef798cc0847646ac37475e3d586f133f0
Can Kologlu <mckologlu@gmail.com>
2023-03-20 17:18:06 -0400

    center content

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import categoryNamesGenerator from "../../helpers/categoryNamesGenerator";
 import Card from "./Card";
 
 export default function Home(props) {
