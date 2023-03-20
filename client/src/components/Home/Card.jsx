19a6f0b0f4421c1398b2c82ada2799cb4d64f4ab
Can Kologlu <mckologlu@gmail.com>
2023-03-20 17:45:31 -0400

    add css to main

--- client/src/components/Home/Card.jsx
+++ client/src/components/Home/Card.jsx
@@ -5,7 +5,7 @@ export default function Card(props) {
   const {category, paragraph, image } = props 
 
   return (
-    <div className="card w-96 bg-base-100 shadow hover:shadow-3xl transition-all">
+    <div className="card w-96 bg-base-100 shadow hover:shadow-3xl transition-all justify-self-center">
 					<Link to={`/categories/${category}`}>
 						<div className="card-body">
 							<h2 className="card-title">I am {category}</h2>
--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -8,7 +8,7 @@ export default function Home(props) {
    const photoLinkArray = [ "https://villa-aventura.com/wp-content/uploads/2022/12/Muziek.jpg","https://t4.ftcdn.net/jpg/02/57/33/93/240_F_257339302_LWVM6ZkukZUoVVo8CY0UHx5y283PG9RR.jpg","https://www.electronicdrumadvisor.com/wp-content/uploads/2022/03/Drumming-Glossary-and-Terms.jpg", "https://images.unsplash.com/photo-1614413970884-6dba7310bd2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Z3VpdGFyJTIwcGVkYWx8ZW58MHx8MHx8&w=1000&q=80"];
 
   const paragraphsArray = ["Don't get strung on else's, get your own strings","For all your key needs, we have the boards","What's more fun than hitting stuff - Flea", "Not satisfied with the natural, get on your game guttural"]
-  console.log("categories are:", categories)
+
 
  const mappedCategories = categories.map((category, index)=> {
   return <Card key={index} category={category} paragraph={paragraphsArray[index]} image={photoLinkArray[index]}/>
@@ -19,7 +19,7 @@ export default function Home(props) {
 			<header>
 				<h1 className="text-5xl text-center my-16">Shop By Category</h1>
 			</header>
-			<div className="flex items-center flex-nowrap justify-center space-x-10 mx-10">
+			<div className="flex items-center flex-nowrap justify-center mx-20 mb-10 grid grid-cols-2 gap-4">
 				{mappedCategories}
 			</div>
 		</div>
