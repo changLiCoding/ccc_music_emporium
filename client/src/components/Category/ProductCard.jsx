9e71b6cc0613e7e06346dd33cab954c4846e3ec7
javascriptsucks <evix1988@live.cn>
2023-03-19 23:14:23 -0600

    before-pull-origin-main

--- client/src/components/Category/ProductCard.jsx
+++ client/src/components/Category/ProductCard.jsx
@@ -3,15 +3,24 @@ import { Link } from "react-router-dom";
 
 export default function Card(props) {
 	return (
-		<div className='card'>
-			<div className='cardbody'>
-				<Link to={"/guitars"}>Guitars</Link>
-				<h2 className='cardtitle'>Title Here!</h2>
-				<p className='carddescription'>Description Here</p>
+		<div className='card w-96 bg-base-100 shadow-xl'>
+			<figure>
+				<img
+					src='/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
+					alt='Shoes'
+				/>
+			</figure>
+			<div className='card-body'>
+				<h2 className='card-title'>
+					Shoes!
+					<div className='badge badge-secondary'>NEW</div>
+				</h2>
+				<p>If a dog chews shoes whose shoes does he choose?</p>
+				<div className='card-actions justify-end'>
+					<div className='badge badge-outline'>Fashion</div>
+					<div className='badge badge-outline'>Products</div>
+				</div>
 			</div>
-			<button className='cardbutton'>
-				I could be a link as "View product"
-			</button>
 		</div>
 	);
 }
