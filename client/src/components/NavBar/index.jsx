86146ed58a6798c0e07562162bf5bbffcec47fd5
Cody Hilborn <codyhilborn11@gmail.com>
2023-03-20 01:19:50 -0400

    Add links to home on navbar

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
 import { faGuitar } from "@fortawesome/free-solid-svg-icons";
 import { faDrum } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function NavBar(props) {
 	const { categories } = props;
@@ -24,13 +25,17 @@ export default function NavBar(props) {
 		<div className="navbar h-24 bg-primary border-solid border-2 border-black">
 			<div className="flex-1 navbar-start">
 				<FontAwesomeIcon icon={faGuitar} size="2x" />
-				<a className="btn btn-ghost normal-case text-2xl">CCC MUSIC EMPORIUM</a>
+				<Link to="/" className="btn btn-ghost normal-case text-2xl">
+					CCC MUSIC EMPORIUM
+				</Link>
 				<FontAwesomeIcon icon={faDrum} size={"2x"} />
 			</div>
 			<div className="navbar-center">
 				<ul className="menu menu-horizontal px-5 ml-20 text-xl">
 					<li>
-						<a>Shop</a>
+          <Link to="/">
+					Shop
+				</Link>
 					</li>
 					<li>
 						<a>About Us</a>
