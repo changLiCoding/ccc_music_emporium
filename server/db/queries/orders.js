82fc0f6581291ec4b7044bc69cedc20845067ef8
javascriptsucks <evix1988@live.cn>
2023-03-21 18:12:15 -0600

    made-checkout-button

--- server/db/queries/orders.js
+++ server/db/queries/orders.js
@@ -2,7 +2,7 @@ const db = require("../connection");
 
 const postOrderAfterPay = (userId, totalInCents, storeId = 1) => {
 	const queryTemplate = `
-    INSERT INTO orders (user_id, store_id, total_in_cents,)
+    INSERT INTO orders (user_id, store_id, total_in_cents)
     VALUES ($1, $2, $3)
     RETURNING *
   `;
@@ -10,7 +10,10 @@ const postOrderAfterPay = (userId, totalInCents, storeId = 1) => {
 
 	return db
 		.query(queryTemplate, queryParams)
-		.then((res) => res.rows)
+		.then((res) => {
+			console.log(res.rows[0]);
+			return res.rows;
+		})
 		.catch((err) => console.error(err.message));
 };
 
--- server/routes/apiCheckoutRoutes.js
+++ server/routes/apiCheckoutRoutes.js
@@ -11,19 +11,39 @@ router.get("/", (req, res) => {
 
 router.post("/", async (req, res) => {
 	try {
-		const userID = req.session.user_id;
+		const { user_id } = req.cookies;
+		const { products } = req.body;
 
-		const amount = req.body.amount;
-		const currency = req.body.currency;
-		const charge = await stripe.charge.create({
-			amount: amount,
-			currency: currency,
-			source: req.body.token,
-		});
-
-		const orderInfo = await postOrderAfterPay(userID, amount);
+		const lineItems = products.map((product) => ({
+			product_id: product.id,
+			price_data: {
+				currency: "usd",
+				product_data: {
+					name: `${product.make} ${product.model}`,
+				},
+				unit_amount: product.price_in_cents,
+			},
+			quantity: 1,
+		}));
+		const orderInfo = await postOrderAfterPay(user_id, amount_in_cents);
 		console.log(orderInfo);
-		res.status(200).json({ success: true, order: orderInfo });
+		console.log(lineItems);
+		const amount_in_cents = products.reduce(
+			(prev, curv) => prev + curv.price_in_cents.parseInt(),
+			0
+		);
+		console.log(amount_in_cents);
+		const session = await stripe.checkout.sessions.create({
+			payment_method_types: ["card"],
+			line_items: lineItems,
+			mode: "payment",
+			success_url: "https://example.com/success",
+			cancel_url: "https://example.com/cancel",
+			metadata: {
+				user_id: user_id,
+			},
+		});
+		res.status(200).json({ sessionId: session.id, order: orderInfo });
 	} catch (error) {
 		res.status(500).json({ error: error.message });
 	}
