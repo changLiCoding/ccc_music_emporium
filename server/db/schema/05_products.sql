46ebe894aa66b402e280b09e3de3072b76b3e49f
javascriptsucks <evix1988@live.cn>
2023-03-24 11:09:39 -0600

    made-change-on-product-schema-add-stock-quantity

--- server/db/schema/05_products.sql
+++ server/db/schema/05_products.sql
@@ -3,10 +3,11 @@ CREATE TABLE products (
   id SERIAL PRIMARY KEY NOT NULL,
   sub_category_id INTEGER NOT NULL REFERENCES sub_categories(id) ON DELETE CASCADE,
   store_id INTEGER NOT NULL REFERENCES stores(id) ON DELETE CASCADE,
-  make VARCHAR(255),
-  model VARCHAR(255),
+  make VARCHAR(255) NOT NUll,
+  model VARCHAR(255) NOT NULL,
   price_in_cents INTEGER NOT NULL,
   rent_rate_in_cents INTEGER NOT NULL,
   description TEXT NOT NULL,
-  image_url VARCHAR(255)
+  image_url VARCHAR(255) NOT NULL,
+  stock_quantity INTEGER NOT NULL
   );
