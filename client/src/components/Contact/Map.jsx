b4bb1c40830a8b5ed6b249384c19641d03066a9d
javascriptsucks <evix1988@live.cn>
2023-03-24 01:18:28 -0600

    made-map-for-stores-locations

--- client/src/components/Contact/Map.jsx
+++ client/src/components/Contact/Map.jsx
@@ -1,62 +1,43 @@
 import React, { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 
+import useStores from "../../hooks/useStores";
+
 mapboxgl.accessToken =
 	"pk.eyJ1IjoibGljaGFuZzAwNCIsImEiOiJjbGZsZTdxajQwMTBkM3Fxem1xOGE4bHZhIn0.NX4YQSsBGEHQ1V_Ytk0Usg";
 
 export default function Map() {
-	const [map, setMap] = useState(null);
-	const [location, setLocation] = useState({
-		longitude: -79.39941593604091,
-		latitude: 43.711066343462775,
-		address: "San Francisco, CA",
-	});
+	const stores = useStores();
+	const mapRef = useRef(null);
 	const openHours = "9am - 5pm";
 	useEffect(() => {
-		// Initialize the map
-		const newMap = new mapboxgl.Map({
-			container: "map",
+		const map = new mapboxgl.Map({
+			container: mapRef.current,
 			style: "mapbox://styles/lichang004/clclili1n000014qtrnk6heqc",
-			center: [location.longitude, location.latitude],
-			zoom: 15,
+			center: [-79.39941593604091, 43.711066343462775],
+			zoom: 8,
 		});
 
-		// Add a marker for the store location
-		new mapboxgl.Marker()
-			.setLngLat([location.longitude, location.latitude])
-			.addTo(newMap);
-
-		// Save the map to state
-		setMap(newMap);
-
-		return () => {
-			// Clean up the map when the component unmounts
-			newMap.remove();
-		};
-	}, [location]);
-
-	useEffect(() => {
-		// Add a popup to the marker with the store open hours
-		if (map) {
-			const popup = new mapboxgl.Popup().setText(`Open hours: ${openHours}`);
-			new mapboxgl.Marker()
-				.setLngLat([location.longitude, location.latitude])
-				.setPopup(popup)
-				.addTo(map);
-		}
-	}, [map, location, openHours]);
+		stores.stores.stores &&
+			stores.stores.stores.forEach((store) => {
+				const marker = new mapboxgl.Marker()
+					.setLngLat([parseFloat(store.longitude), parseFloat(store.latitude)])
+					.setPopup(
+						new mapboxgl.Popup().setHTML(`
+		            <div>
+		              <h3>${store.name}</h3>
+		              <p>${store.location}</p>
+		              <p>Open hours: ${openHours}</p>
+		            </div>
+		          `)
+					)
+					.addTo(map);
+			});
+	}, [stores.stores.stores]);
 
 	return (
-		<div className='flex'>
-			<div>
-				<h2>Store Name</h2>
-				<p>Address: 123 Main St</p>
-				<p>Phone: 555-555-5555</p>
-				<p>Hours: Monday - Friday 9am - 5pm</p>
-			</div>
-			<div
-				id='map'
-				style={{ width: "600px", height: "400px" }}></div>
-		</div>
+		<div
+			ref={mapRef}
+			style={{ width: "60rem", height: "25rem" }}></div>
 	);
 }
--- client/src/components/Contact/index.jsx
+++ client/src/components/Contact/index.jsx
@@ -4,24 +4,24 @@ import Map from "./Map";
 
 const Contact = () => {
 	return (
-		<div className='flex flex-col w-full border-opacity-50'>
+		<div className='flex flex-col w-full border-opacity-50 container'>
 			<div className='grid h-20 card bg-base-300 rounded-box place-items-center'>
 				Stores Infomation
 			</div>
 			<div className='divider'>Locations</div>
 			<div className='grid card bg-base-300 rounded-box place-items-center'>
-				<Map />
+				<div className='flex w-full justify-evenly'>
+					<div>
+						<h2>Store Name</h2>
+						<p>Address: 123 Main St</p>
+						<p>Phone: 555-555-5555</p>
+						<p>Hours: Monday - Friday 9am - 5pm</p>
+					</div>
+					<Map />
+				</div>
 			</div>
 		</div>
 	);
 };
 
 export default Contact;
-
-// {/* <div className='flex w-full flex-col justify-center'>
-//   <h2 className='text-xl'>
-//     Heading
-//     <span className='badge badge-lg'>NEW</span>
-//   </h2>
-//
-// </div> */}
