4dc2e184eac5136842ab3246503682bcea8aaddb
javascriptsucks <evix1988@live.cn>
2023-03-25 00:26:37 -0600

    fix rent style

--- client/src/components/Category/ProductRentCalendar.jsx
+++ client/src/components/Category/ProductRentCalendar.jsx
@@ -49,31 +49,15 @@ export default function ProductRentCalendar({ product, setProducts }) {
 						value={value}
 						onChange={handleValueChange}
 					/>
-					<div>
+					<div className='flex justify-center mt-7'>
 						<img
+							className='w-2/3'
 							src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZC4-BVm_0r-m6Dey0HXbywBtn6tLM37Bsew&usqp=CAU'
 							alt='band here'
 						/>
 					</div>
 					{/* <div className='modal-action mt-80 flex justify-between'> */}
-					<div className='modal-action mt-40 flex justify-between'>
-						<div className='scale-100'>
-							<p className='transition delay-150 duration-300 ease-in text-4xl'>
-								{randomFacts[1]}
-							</p>
-						</div>
-						<div>
-							<div>
-								<span className='text-lg'>
-									{value.startDate &&
-										value.endDate &&
-										priceConverter(
-											getDaysDifference(value.startDate, value.endDate) *
-												product.rent_rate_in_cents
-										)}
-								</span>
-							</div>
-						</div>
+					<div className='modal-action mt-10 flex justify-between align-baseline'>
 						<button
 							disabled={product.stock_quantity === 0}
 							type='button'
@@ -89,14 +73,15 @@ export default function ProductRentCalendar({ product, setProducts }) {
 							}}>
 							Submit
 						</button>
-						<span>
-							{value.startDate &&
-								value.endDate &&
-								priceConverter(
-									getDaysDifference(value.startDate, value.endDate) *
-										product.rent_rate_in_cents
-								)}
+						<span className='badge badge-lg self-center'>
+							{value.startDate
+								? `Rent Spend: 	${priceConverter(
+										getDaysDifference(value.startDate, value.endDate) *
+											product.rent_rate_in_cents
+								  )}`
+								: "Please Select Dates"}
 						</span>
+
 						<label
 							htmlFor='calendar'
 							className='btn'>
