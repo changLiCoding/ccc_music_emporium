52105d293eaf1b3abb0c4978ab6f66630753b593
javascriptsucks <evix1988@live.cn>
2023-03-25 17:40:11 -0600

    remove cart now will increse stock as well

--- client/src/contexts/ProductContext.js
+++ client/src/contexts/ProductContext.js
@@ -16,29 +16,30 @@ export function ProductProvider(props) {
 		updatedType,
 		message
 	) {
-		let productToUpdate = {};
-		setProducts((prevProducts) => {
-			console.log(cartProduct);
-			const updatedProducts = { ...prevProducts };
-			console.log(updatedProducts.products);
-			productToUpdate = updatedProducts.products.find(
-				(product) => product.model === cartProduct.model
-			);
-			console.log(productToUpdate);
-			if (updatedType === "increment") {
-				productToUpdate.stock_quantity = productToUpdate.stock_quantity + 1;
-			} else if (updatedType === "decrement") {
-				productToUpdate.stock_quantity = productToUpdate.stock_quantity - 1;
-			}
-			console.log("Here is changed product object ::", productToUpdate);
-
-			return updatedProducts;
-		});
 		try {
+			let productToUpdate = {};
+			await setProducts((prevProducts) => {
+				console.log(cartProduct);
+				const updatedProducts = { ...prevProducts };
+				console.log(updatedProducts.products);
+				productToUpdate = updatedProducts.products.find(
+					(product) => product.model === cartProduct.model
+				);
+				console.log(productToUpdate);
+				if (updatedType === "increment") {
+					productToUpdate.stock_quantity = productToUpdate.stock_quantity + 1;
+				} else if (updatedType === "decrement") {
+					productToUpdate.stock_quantity = productToUpdate.stock_quantity - 1;
+				}
+				console.log("Here is changed product object ::", productToUpdate);
+				console.log(productToUpdate.category_name);
+				return updatedProducts;
+			});
+			console.log("productToUpdate outside of there: ", productToUpdate);
 			const response = await axios.post(
 				`http://localhost:8080/api/categories/${productToUpdate.category_name}`,
 				{
-					productToUpdate,
+					product: productToUpdate,
 					updateType: updatedType,
 				}
 			);
