81f7d8642c2f797c1d7de82d0b9b82780584a757
javascriptsucks <evix1988@live.cn>
2023-03-25 00:35:00 -0600

    adding background to rend

--- client/src/components/Category/ProductRentCalendar.jsx
+++ client/src/components/Category/ProductRentCalendar.jsx
@@ -49,15 +49,15 @@ export default function ProductRentCalendar({ product, setProducts }) {
 						value={value}
 						onChange={handleValueChange}
 					/>
-					<div className='flex justify-center mt-7'>
+					<div className='flex justify-center mt-3'>
 						<img
 							className='w-2/3'
-							src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZC4-BVm_0r-m6Dey0HXbywBtn6tLM37Bsew&usqp=CAU'
+							src='https://cdn.pixabay.com/photo/2017/06/21/22/40/guitar-2428921_1280.jpg'
 							alt='band here'
 						/>
 					</div>
 					{/* <div className='modal-action mt-80 flex justify-between'> */}
-					<div className='modal-action mt-10 flex justify-between align-baseline'>
+					<div className='modal-action mt-7 flex justify-between align-baseline'>
 						<button
 							disabled={product.stock_quantity === 0}
 							type='button'
