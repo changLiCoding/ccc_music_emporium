ebe6a6a5b47c76ef092b3ab03889d00fffd1efdc
Cody Hilborn <codyhilborn11@gmail.com>
2023-03-27 15:39:37 -0400

    Add header to home page, reformat cards

--- client/src/components/Home/Card.jsx
+++ client/src/components/Home/Card.jsx
@@ -5,19 +5,19 @@ export default function Card(props) {
 	const { category, paragraph, image } = props;
 
 	return (
-		<div className='card w-full bg-base-100 shadow hover:shadow-3xl transition-all'>
+		<div className="card w-full bg-white shadow hover:shadow-3xl transition-all">
 			<Link to={`/categories/${category}`}>
-				<div className='card-body p-6'>
-					<h2 className='card-title text-xl font-bold'>
+				<div className="card-body p-6 flex items-center">
+					<h2 className="card-title text-3xl font-bold underline">
 						{category.charAt(0).toUpperCase() + category.slice(1)}
 					</h2>
-					<p className='mt-4'>{paragraph}</p>
+					<p className="mt-3 italic">{paragraph}</p>
 				</div>
-				<figure className='relative h-60'>
+				<figure className="relative h-60">
 					<img
-						className='object-cover w-full h-full'
+						className="object-cover w-full h-full"
 						src={image}
-						alt='Keyboard'
+						alt="Keyboard"
 					/>
 				</figure>
 			</Link>
